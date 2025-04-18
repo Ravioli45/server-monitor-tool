44c380c730f7f895811c9a6977105d4c54cdc816
Viktor Johansen <viktorjohansen@outlook.com>
2025-04-18 18:06:11 -0400

    Added invalid login test

--- tests/test_project.py
+++ tests/test_project.py
@@ -8,10 +8,17 @@ def test_home(client):
 
 
 def test_registration(client, app):
-    response = client.post("/", data={"email": "test@test.com", "password": "testpassword"})
+    response = client.post("/register", data={"email": "test@test.com", "password": "testpassword"})
     
     with app.app_context():
         user = db.session.get(User, 1)
 
         assert User.query.count() == 1
         assert User.query.first().email == "viktorjohansen@outlook.com"
+
+def test_invalid_login(client):
+    client.post("/login", data={"email": "testss@test.com", "password": "testpassword"})
+
+    response = client.get("/dashboard")
+
+    assert response.status_code == 302
